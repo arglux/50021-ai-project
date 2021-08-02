b7af4080879e454201f793a6caa709977e78564c
heavensward.argentlux <heavensward.argentlux@gmail.com>
2021-08-02 19:59:36 +0800

    updated train, test

--- test.py
+++ test.py
@@ -1,7 +1,22 @@
-from model import Net
+from model import Net, LinReg2
 
 import torch
 import pandas as pd
+
+
+def train(train_data, model):
+	model.eval()
+	for _X, _y in test_dl:
+		test_batch_losses = []
+		_X = Variable(_X).cuda().float()
+		_y = Variable(_y).cuda().float()
+
+		#apply model
+		test_preds = model(_X)
+		test_loss = criterion(test_preds, _y)
+
+		for i in range(100): print(math.floor(10**_y[i]-1),math.floor(10**test_preds[i].item()-1))
+		break
 
 def sensitivity_evaluation(model, test_data, test_label, device='cpu'):
 	TP = 0
@@ -31,18 +46,19 @@ def sensitivity_evaluation(model, test_data, test_label, device='cpu'):
 	print("Class specific accuracy of correctly predicting a hit song is {0}".format(rate))
 
 if __name__ == '__main__':
-	inp_size = 49
-	out_size = 1
-	device = 'cpu'
+	print("OK")
+	# inp_size = 49
+	# out_size = 1
+	# device = 'cpu'
 
-	trained_model = Net(inp_size, out_size).to(device) # reinitialize model
-	trained_model.load_state_dict(torch.load('./models/dummy-model-2706-2356'))
-	trained_model.eval()
+	# trained_model = Net(inp_size, out_size).to(device) # reinitialize model
+	# trained_model.load_state_dict(torch.load('./models/dummy-model-2706-2356'))
+	# trained_model.eval()
 
-	test_data = pd.read_csv('./data/herremans_hit_1030test.csv')
-	print(f"Data loaded. Train data shape: {test_data.shape}")
+	# test_data = pd.read_csv('./data/herremans_hit_1030test.csv')
+	# print(f"Data loaded. Train data shape: {test_data.shape}")
 
-	x = torch.FloatTensor(test_data.loc[:, test_data.columns != 'Topclass1030'].values).to(device)
-	y = torch.FloatTensor(test_data['Topclass1030']).to(device)
+	# x = torch.FloatTensor(test_data.loc[:, test_data.columns != 'Topclass1030'].values).to(device)
+	# y = torch.FloatTensor(test_data['Topclass1030']).to(device)
 
-	sensitivity_evaluation(trained_model, x, y)
+	# sensitivity_evaluation(trained_model, x, y)
--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from model import Net
+from model import Net, LinReg2
 from save import save
 
 import torch
@@ -6,7 +6,33 @@ import torch.nn as nn
 
 import pandas as pd
 
-def train(x, y, model, loss_function, optimizer, device, epochs=1000+1):
+class RMSLELoss(nn.Module):
+	def __init__(self):
+		super().__init__()
+		self.mse = nn.MSELoss()
+
+	def forward(self, pred, actual):
+		return torch.sqrt(self.mse(torch.log(pred + 1), torch.log(actual + 1)))
+
+def train(train_data, model, num_epochs):
+	for e in range(num_epochs):
+		batch_losses = []
+		for i, (Xb, yb) in enumerate(train_data, model):
+			_X = Variable(Xb).cuda().float()
+			_y = Variable(yb).cuda().float()
+			preds = model(_X)
+			loss = criterion(preds, _y)
+
+			optimizer.zero_grad()
+			loss.backward()
+			optimizer.step()
+			batch_losses.append(loss.item())
+
+		mbl = np.mean(np.sqrt(batch_losses)).round(3)
+
+		if e % 5 == 0: print("Epoch [{}/{}], Batch loss: {}".format(e, num_epochs, mbl))
+
+def train_dummy(x, y, model, loss_function, optimizer, device, epochs=1000+1):
 	for epoch in range(epochs):
 		features = x.to(device)
 		target = y.to(device)
@@ -22,21 +48,30 @@ def train(x, y, model, loss_function, optimizer, device, epochs=1000+1):
 		    print (f"Epoch: {epoch}, Loss: {loss}")
 
 if __name__ == "__main__":
-	inp_size = 49
-	out_size = 1
-	lr_rate = 0.001
-	device = "cuda"
+	input_size = 65
+	hidden_size = 10
+	output_size = 1
+	learning_rate = 0.01
+	num_epochs = 50
 
-	model = Net(inp_size, out_size).to(device)
-	loss_function = nn.BCELoss()
-	optimizer = torch.optim.Adam(model.parameters(), lr=lr_rate)
+	model = LinReg2(input_size, hidden_size, output_size)
+	criterion = RMSLELoss()
+	optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-	# assuming the data is downloaded and saved in the same dir -> ./content
-	train_data = pd.read_csv('./data/herremans_hit_1030training.csv')
-	print(f"Data loaded. Train data shape: {train_data.shape}")
+	if torch.cuda.is_available():
+		model.cuda()
+		print(model, criterion, optimizer)
 
-	x = torch.FloatTensor(train_data.loc[:, train_data.columns != 'Topclass1030'].values).to(device)
-	y = torch.FloatTensor(train_data['Topclass1030']).to(device)
+	# model = Net(inp_size, out_size).to(device)
+	# loss_function = nn.BCELoss()
+	# optimizer = torch.optim.Adam(model.parameters(), lr=lr_rate)
 
-	train(x, y, model, loss_function, optimizer, device)
-	save(model, 'dummy-model')
+	# # assuming the data is downloaded and saved in the same dir -> ./content
+	# train_data = pd.read_csv('./data/herremans_hit_1030training.csv')
+	# print(f"Data loaded. Train data shape: {train_data.shape}")
+
+	# x = torch.FloatTensor(train_data.loc[:, train_data.columns != 'Topclass1030'].values).to(device)
+	# y = torch.FloatTensor(train_data['Topclass1030']).to(device)
+
+	# train(x, y, model, loss_function, optimizer, device)
+	# save(model, 'dummy-model')
